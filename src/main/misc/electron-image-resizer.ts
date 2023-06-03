29b3ee3b10a4e670b24c93adfc7ca942a79617d9
Adedoyin-Emmanuel <adedoyine535@gmail.com>
2023-06-04 00:38:56 +0100

    feat: Added the image-event listeners

--- src/main/misc/electron-image-resizer.ts
+++ src/main/misc/electron-image-resizer.ts
@@ -1,9 +1,11 @@
 const { ipcMain, dialog, app } = require('electron');
 const fs = require('fs');
 const path = require('path');
+const sharp = require('sharp');
 
-ipcMain.on('save-file', (event, prams:any) => {
-  const {files, fileName} = prams;
+/*Define the utility function to save files*/
+ipcMain.on('save-file', (event, prams: any) => {
+  const { files, fileName } = prams;
   const savePath = path.resolve(app.getPath('downloads'), fileName);
 
   fs.writeFile(savePath, files, (error: any) => {
@@ -19,4 +21,52 @@ ipcMain.on('save-file', (event, prams:any) => {
       });
     }
   });
+});
+
+/*listen for image-rotate events in the renderer process*/
+ipcMain.on('rotate-image', (event, prams: any) => {
+  const { file, rotationAngle } = prams;
+
+  const buffer = Buffer.from(file, 'base64');
+  sharp(buffer)
+    .rotate(rotationAngle)
+    .toBuffer()
+    .then((rotatedBuffer: Buffer) => {
+      event.sender.send('image-rotated', rotatedBuffer);
+    })
+    .catch((error: any) => {
+      event.sender.send('rotate-image-error', error);
+    });
+});
+
+/*listen for image-brightness events in the renderer process*/
+ipcMain.on('adjust-brightness', (event, prams: any) => {
+  const { file, brightnessValue } = prams;
+  const buffer = Buffer.from(file, 'base64');
+
+  sharp(buffer)
+    .modulate({ brightness: brightnessValue })
+    .toBuffer()
+    .then((buffer: Buffer) => {
+      event.sender.send('image-brightned', buffer);
+    })
+    .catch((error: any) => {
+      event.sender.send('brightness-adjust-error', error);
+    });
+});
+
+/*listen for image-resize events in the renderer process*/
+ipcMain.on('resize-image', (event, prams: any) => {
+  const { file, height, width } = prams;
+  const buffer = Buffer.from(file, 'base64');
+
+  sharp(buffer)
+    .resize({ height, width })
+    .toBuffer()
+    .then((buffer: Buffer) => {
+      event.reply('image-resized', buffer);
+    })
+    .catch((error: string) => {
+      event.sender.send('resize-image-error', error);
+    });
 });
