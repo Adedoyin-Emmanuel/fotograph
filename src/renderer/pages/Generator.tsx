8c2a63d362b317ccdc77c6bf969e2a2c9b7931eb
Adedoyin-Emmanuel <adedoyine535@gmail.com>
2023-06-07 01:32:39 +0100

    Updated the Bg Remover screen

--- src/renderer/pages/Generator.tsx
+++ src/renderer/pages/Generator.tsx
@@ -2,13 +2,19 @@ import React from 'react';
 import AppLayout from 'renderer/components/layouts/AppLayout';
 import AppHeader from 'renderer/components/common/Header';
 import AppButton from 'renderer/components/common/Button';
+import $ from 'jquery';
 
 const Generator = (): JSX.Element => {
   return (
     <React.Fragment>
-      <AppLayout className="app-content-scroll app-content-flex flex-column" onGeneratorPage={true}>
+      <AppLayout
+        className="app-content-scroll app-content-flex flex-column"
+        onGeneratorPage={true}
+      >
         <section className="convert-container d-flex align-items-start justify-content-start flex-column mx-4">
-          <AppHeader className="text-capitalize my-3">Generate Images</AppHeader>
+          <AppHeader className="text-capitalize my-3">
+            Generate Images
+          </AppHeader>
           <p className="text-muted">Generate any image using AI</p>
         </section>
 
--- src/renderer/pages/Remover.tsx
+++ src/renderer/pages/Remover.tsx
@@ -1,13 +1,51 @@
-import React from 'react';
+import React, { DragEvent, useState, FormEvent } from 'react';
 import AppLayout from 'renderer/components/layouts/AppLayout';
 import AppHeader from 'renderer/components/common/Header';
 import AppButton from 'renderer/components/common/Button';
 import AppFileDropZone from 'renderer/components/common/AppFileDropZone';
 
 const Remover = (): JSX.Element => {
+  const [dragging, setDragging] = useState<boolean>(false);
+  const [fileToRemoveBg, setFileToRemoveBg] = useState<any[]>();
+
+  const handleFileUpload = (e: any) => {
+    e.preventDefault();
+    if (e.dataTransfer?.files) {
+      const files = e.dataTransfer?.files;
+      const fileArray = [...files];
+      setFileToRemoveBg(fileArray);
+    } else {
+      const files = e?.target.files || fileToRemoveBg;
+      const fileArray = [...files];
+      setFileToRemoveBg(fileArray);
+    }
+  };
+
+  const handleDragEnter = (e: DragEvent<HTMLDivElement>) => {
+    e.preventDefault();
+    setDragging(true);
+  };
+
+  const handleDragLeave = (e: DragEvent<HTMLDivElement>) => {
+    e.preventDefault();
+    setDragging(false);
+  };
+
+  const handleDragOver = (e: DragEvent<HTMLDivElement>) => {
+    e.preventDefault();
+  };
+
+  const handleDrop = (e: DragEvent<HTMLDivElement>) => {
+    e.preventDefault();
+    setDragging(false);
+    handleFileUpload(e);
+  };
   return (
     <React.Fragment>
-      <AppLayout className="app-content-scroll app-content-flex flex-column" onRemoverPage={true}>
+      <AppLayout
+        className="app-content-scroll app-content-flex flex-column"
+        onRemoverPage={true}
+      >
         <section className="convert-container d-flex align-items-start justify-content-start flex-column mx-4">
           <AppHeader className="text-capitalize my-3">
             Image background Remover
@@ -21,9 +59,14 @@ const Remover = (): JSX.Element => {
           className="upload-button-section width-toggle-3 d-flex align-items-center m-auto"
           id="conversion_form"
           encType="multipart/form-data"
+          onChange={(e: FormEvent) => handleFileUpload(e)}
         >
-        <AppFileDropZone/>
-
+          <AppFileDropZone
+            handleDragEnter={handleDragEnter}
+            handleDragLeave={handleDragLeave}
+            handleDragOver={handleDragOver}
+            handleDrop={handleDrop}
+          />
         </form>
       </AppLayout>
     </React.Fragment>
